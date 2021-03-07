928ef65b201e0671c03db431fe0232a9ea5c6114
katta-sravani14 <kattasravani413@gmail.com>
2021-03-07 23:08:50 +0530

    added

---
+++ EmployeePayRollData/src/test/java/com/bridgelabz/EmployeePayRollData/LibraryTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz.EmployeePayRollData;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LibraryTest {
+    @Test public boolean testSomeLibraryMethod() {
+        LibraryTest classUnderTest = new LibraryTest();
+        Assert.assertTrue("someLibraryMethod should return 'true'", classUnderTest.testSomeLibraryMethod());
+		return false;
+    }
+
+	private void assertTrue(String message, Object testSomeLibraryMethod) {
+		// TODO Auto-generated method stub
+		
+	}
+}
